8fbb2aa04c8cf518186a6703b48f88c5f0de0187
Dcolor Wei <corfer@yeah.net>
2022-11-21 18:17:30 +0800

    feature: 首页page

---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,22 @@
+// index.ts
+// 获取应用实例
+const app = getApp<IAppOption>()
+
+Page({
+  data: {
+    active: 0,
+    tarbar: [
+      { item: "首页", icon: "home" },
+      { item: "签证", icon: "visa" },
+      { item: "购物车", icon: "cart" },
+      { item: "我的", icon: "user" },
+    ]
+  },
+  onChange(event: any) {
+    // event.detail 的值为当前选中项的索引
+    this.setData({ active: event.detail });
+    wx.setNavigationBarTitle({
+      title: this.data.tarbar[this.data.active].item
+    })
+  },
+});
