f7f6eee41659dc60f994461aedd4f688219b95a4
Dcolor Wei <corfer@yeah.net>
2022-11-21 22:59:47 +0800

    feature: 商务合作表单页面

---
+++ miniprogram/pages/user-cop/user-cop.ts
@@ -0,0 +1,12 @@
+/* eslint-disable promise/catch-or-return */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable promise/always-return */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+Page({
+    commit() {
+        //提交成功的回调
+        wx.navigateTo({ url: "/pages/user-cop-commit/user-cop-commit" })
+    }
+});
