2c36b823831daee725fea72cce249dd0b451b635
Dcolor Wei <corfer@yeah.net>
2022-11-21 18:17:49 +0800

    feature: 用户页首页

---
+++ miniprogram/pages/user/user.ts
@@ -0,0 +1,12 @@
+/* eslint-disable promise/catch-or-return */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable promise/always-return */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+Page({
+    toOrder() {
+        console.log("aaa")
+        wx.navigateTo({ url: "/pages/user-order/user-order" })
+    }
+});
