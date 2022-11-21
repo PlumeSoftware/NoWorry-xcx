7af307614c60617905072512c622c7750074c59a
Dcolor Wei <corfer@yeah.net>
2022-11-21 18:19:54 +0800

    feature: 用户-订单详情

---
+++ miniprogram/pages/user-order-detail/user-order-detail.ts
@@ -0,0 +1,10 @@
+/* eslint-disable promise/catch-or-return */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable promise/always-return */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+Page({
+    data: {
+    }
+});
