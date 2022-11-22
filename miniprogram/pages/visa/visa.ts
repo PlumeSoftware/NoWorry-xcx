b8eb79b7d8d7f78c3033cf6d7b53f40679e8db5e
Dcolor Wei <corfer@yeah.net>
2022-11-22 19:25:36 +0800

    feature: 签证页面

---
+++ miniprogram/pages/visa/visa.ts
@@ -0,0 +1,19 @@
+/* eslint-disable promise/catch-or-return */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable promise/always-return */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+Component({
+    data: {
+        activeVisa: 0,
+        show: false
+    },
+    lifetimes: {
+        ready() {
+            //修复vant-tab渲染延时错误
+            // @ts-ignore
+            setTimeout(()=>{this.setData({ show: true })},2500)
+        }
+    }
+});
