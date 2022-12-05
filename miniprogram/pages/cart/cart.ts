f38c920d761e42ac92d35443c23028e97d41151e
Dcolor Wei <corfer@yeah.net>
2022-12-05 20:20:59 +0800

    feature: 购物车优化

--- miniprogram/pages/cart/cart.ts
+++ miniprogram/pages/cart/cart.ts
@@ -6,66 +6,59 @@
 
 Page({
     data: {
-        carts: [
-            {
-                title: "美国签证顺位",
-                describe: "顺位预约，包含材料",
-                price: 46,
-                quantity: 1,
-                select: false
-            },
-            {
-                title: "美国签证顺位",
-                describe: "顺位预约，包含材料",
-                price: 46,
-                quantity: 1,
-                select: false
-            },
-            {
-                title: "美国签证顺位",
-                describe: "顺位预约，包含材料",
-                price: 48,
-                quantity: 1,
-                select: false
-            },
-
-        ],
+        carts: [] as any,
         allselect: false,
         totalPrice: 0,
-        totalPrice2: 0
+        totalPrice2: 8.6231
     },
+
+
+    updateCart() {
+        console.log("assa")
+        this.setData({ carts: wx.getStorageSync('carts') })
+    },
+
     checkBtn(e: any) {
         const index = e.currentTarget.dataset.index
         const carts = this.data.carts;
         carts[index].select = !carts[index].select
-        if (carts.findIndex(item => !item.select) == -1) {
+
+        this.setAllSelect()
+        this.culTotal()
+    },
+
+    setAllSelect() {
+        const carts = this.data.carts;
+        if (carts.findIndex((item: any) => !item.select) == -1) {
             this.setData({ allselect: true })
         } else {
             this.setData({ allselect: false })
         }
-
         this.setData({ carts: carts })
-        this.culTotal()
     },
 
     add(e: any) {
-        console.log(e)
         const index = e.currentTarget.dataset.index
         const add = e.currentTarget.dataset.add
         const carts = this.data.carts;
         carts[index].quantity = carts[index].quantity + add
+        carts[index].quantity = carts[index].quantity ? carts[index].quantity : 1;
+        carts[index].quantity = carts[index].quantity < 9 ? carts[index].quantity : 9
         this.setData({ carts: carts })
         this.culTotal()
     },
 
+
+    //重新计算价格
     culTotal() {
         let total = 0;
         const carts = this.data.carts;
-        carts.forEach(item => {
+        carts.forEach((item: any) => {
             total += item.select ? item.price * item.quantity : 0
         })
-        const total2 = Number((total * 8.6231).toFixed(2));
+        const total2 = 8.6231;
         this.setData({ totalPrice: total, totalPrice2: total2 })
+        wx.setStorageSync('carts', carts)
     }
 
 });
