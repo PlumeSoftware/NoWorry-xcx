62ab30a790976b133468450c4dca94c8dd997a26
惟泪千行 <@LAPTOP-UEG40MJQ>
2022-12-08 23:14:11 +0800

    feature:home的UI设计

--- miniprogram/pages/home/home.ts
+++ miniprogram/pages/home/home.ts
@@ -3,7 +3,23 @@
 /* eslint-disable @typescript-eslint/comma-dangle */
 /* eslint-disable promise/always-return */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-
 Page({
-    
+    data: {
+        coverUrlList: [
+            "/static/home/account1.png",
+            "/static/home/account2.png",
+            "/static/home/account3.png",
+            "/static/home/account4.png"
+        ],
+        coverIndex: 0
+    },
+    //事件 显示时触发
+
+    //页面渲染完毕
+    start() {
+        console.log("aaa")
+        setInterval(() => {
+            this.setData({ coverIndex: (this.data.coverIndex + 1) % this.data.coverUrlList.length })
+        }, 3000)
+    }
 });
