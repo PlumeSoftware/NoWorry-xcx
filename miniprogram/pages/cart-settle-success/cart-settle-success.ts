3310e389768b26ba696c91c9c630fdcec9d78b36
Dcolor Wei <corfer@yeah.net>
2022-12-09 22:54:25 +0800

    style: 购物车结算更新

---
+++ miniprogram/pages/cart-settle-success/cart-settle-success.ts
@@ -0,0 +1,12 @@
+/* eslint-disable promise/catch-or-return */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable promise/always-return */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+Page({
+
+    toHome(){
+        wx.reLaunch({url:"/pages/index/index"})
+    }
+});
