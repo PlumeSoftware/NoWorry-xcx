3a3867cc13f7792d8488ba95e823c3a320f6e94d
惟泪千行 <@LAPTOP-UEG40MJQ>
2022-12-12 21:22:39 +0800

    "feature:用户协议和隐私协议创建"

---
+++ miniprogram/pages/user-set-notice/notice.ts
@@ -0,0 +1,28 @@
+/* eslint-disable promise/catch-or-return */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable promise/always-return */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+Page({
+    data: {
+        paragraph1:[
+            "1. 在申请人未完成递签时间预约时，可以无理由申请100%退款。",
+            "2. 在申请人完成递签时间预约时，如果中途需要更改国家/时间（包括刷位），需要向NoWorry签证支付28磅手续费。",
+            "3. 在申请人完成递签时间预约时，如果中途需要取消预约签证，费用将不予退还。",
+            "4.其他由于申请人自己的原因所导致的NoWorry人力的付出，需要向NoWorry签证支付相应的费用的情况，费用将由双方协定。",
+        ],
+        paragraph2:[
+            "1. 若申请人因我们工作人员失误而造成拒签（可能性为0.1%），则全额退款服务费100%。",
+            "2. 若申请人个人材料存在以下几种情况则不予退款：",
+        ],
+        paragraph3:[
+            "(1)提供虚假材料或未在规定时间内提供制定材料；",
+            "(2)未按照我们的要求私自递不符合规定的材料；",
+            "(3)未在规定的面签时间前往相应地点办理导致的；",
+            "(4)隐瞒真实访问目的，拒签信中明确指出怀疑申请人此次访问目的的；",
+            "(5)申请人主观要求撤签而导致签证被退回的；",
+            "(6)其他由于申请人自身原因所导致的拒签情况。",
+        ]
+    }
+});
