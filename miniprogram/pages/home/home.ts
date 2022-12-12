a4acac2b64a958d87a968137c64794e25678459f
惟泪千行 <@LAPTOP-UEG40MJQ>
2022-12-12 14:20:45 +0800

    feature: 首页跳转至海报

--- miniprogram/pages/home/home.ts
+++ miniprogram/pages/home/home.ts
@@ -3,6 +3,7 @@
 /* eslint-disable @typescript-eslint/comma-dangle */
 /* eslint-disable promise/always-return */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as http from "../../utils/http"
 Page({
     data: {
         coverUrlList: [
@@ -15,36 +16,61 @@ Page({
 
         VisaList:[
             {
+                commodityId:1,
                 commodityName:"法国签证刷签代办",
                 commodityPrice:128,
                 commodityCover:"/static/home/French.png"
             },
             {
+                commodityId:1,
                 commodityName:"西班牙签证刷签代办",
                 commodityPrice:128,
                 commodityCover:"/static/home/Spain.png"
             },
             {
+                commodityId:1,
                 commodityName:"美国签证刷签代办",
                 commodityPrice:88,
                 commodityCover:"/static/home/USA.png"
             },
             {
+                commodityId:1,
                 commodityName:"美国签证刷签代办",
                 commodityPrice:88,
                 commodityCover:"/static/home/USA.png"
             },
             {
+                commodityId:1,
                 commodityName:"美国签证刷签代办",
                 commodityPrice:88,
                 commodityCover:"/static/home/USA.png"
             },
         ]
+
+    },
+    toPoster(e:any) {
+        console.log(e)
+        wx.navigateTo({url:'/pages/home-poster/poster'+"?posterId="+e.currentTarget.dataset["id"]})
+    },
+    toDetail(e:any) {
+        const commodityId = e.currentTarget.dataset.commodityid
+        console.log( e.currentTarget)
+        wx.navigateTo({ url: '/pages/visa-detail/visa-detail' + `?commodityId=${commodityId}` })
+    },
+
+    drag(e){
+        console.log(e)
     },
     //事件 显示时触发
 
     //页面渲染完毕
     start() {
+        wx.request({
+            url: http.BASE_URL + `/visa/group/11`,
+            success: (res) => {
+                this.setData({ visaList: res.data })
+            }
+        })
         wx.pageScrollTo({})
         console.log("aaa")
         setInterval(() => {
