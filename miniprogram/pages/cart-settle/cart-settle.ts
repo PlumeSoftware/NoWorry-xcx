a99f08db1551e120304dac31611128bd53d29aa9
Dcolor Wei <corfer@yeah.net>
2022-12-13 13:40:27 +0800

    feature: 接口使用entity

--- miniprogram/pages/cart-settle/cart-settle.ts
+++ miniprogram/pages/cart-settle/cart-settle.ts
@@ -4,6 +4,9 @@
 /* eslint-disable promise/always-return */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
+import { Cart } from "miniprogram/entity/cart";
+import { CartDetail} from "miniprogram/entity/order";
+
 Page({
     data: {
         userName: '',
@@ -56,9 +59,33 @@ Page({
             })
         }, 2400)
 
+        const orderDetail:CartDetail[] = []
+        const paid = this.data.carts;
+        console.log("paid", paid)
+        paid.forEach((cart: Cart) => {
+            if (cart.select) {
+                orderDetail.push({
+                    boughtQuantity: cart.quantity,
+                    invPrice: cart.quantity * cart.currentPrice,
+                    commodityId: cart.commodityId
+                })
+            }
+        })
+        wx.request({
+            url: "http://127.0.0.1:3000/v1/mp/order/submit",
+            method: "POST",
+            data:
+            {
+                token: wx.getStorageSync('token'),
+                orderTotalPrice: this.data.totalPrice,
+                payWay: 1,
+                orderDetail: orderDetail
+            }
+        })
+
+
         //删除购物车已支付商品
         const carts = wx.getStorageSync('carts');
-        const paid = this.data.carts;
         for (let i = 0; i < carts.length; i++) {
             if (paid.findIndex((item: { commodityId: number }) => carts[i].commodityId == item.commodityId) != -1) {
                 carts.splice(i--, 1)
