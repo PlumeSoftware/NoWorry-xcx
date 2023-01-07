414664cfda67487359f165ed405a00c196772202
Dcolor Wei <corfer@yeah.net>
2023-01-07 14:12:47 +0800

    fix: 显示调整

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,4 +1,17 @@
 // app.ts
 App<IAppOption>({
   globalData: {},
+  onLaunch() {
+    wx.showLoading({
+      title: '加载中',
+      mask:true
+    })
+
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 1000)
+    
+    const updateManager = wx.getUpdateManager()
+    updateManager.applyUpdate()
+  }
 })
