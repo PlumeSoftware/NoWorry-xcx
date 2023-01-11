6c84fe5e0081115db5097f7b4eed6adf3a2c45a0
-h <corfer@yeah.net>
2023-01-11 18:00:51 +0800

    refactor: 将所有getstorage变为getApp().globalData

--- miniprogram/pages/visa/visa.ts
+++ miniprogram/pages/visa/visa.ts
@@ -4,7 +4,8 @@
 /* eslint-disable promise/always-return */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-import { webGet } from "../../utils/http"
+import { Visa } from "../../entity/visa";
+import { webGet} from "../../utils/http"
 Component({
     data: {
         activeVisa: 0,
@@ -15,33 +16,30 @@ Component({
         ]
     },
 
-    
+
     methods: {
-        inp(){},
+        inp() { },
         toDetail(e: any) {
             const commodityId = e.currentTarget.dataset.commodityid
             wx.navigateTo({ url: '/pages/visa-detail/visa-detail' + `?commodityId=${commodityId}` })
         },
-        onTap(){
-           
+        onTap() {
+
         }
 
     },
     lifetimes: {
         async ready() {
             const visaList = []
-            visaList.push(await webGet(`/visa/group/hot`))
-            visaList.push(await webGet(`/visa/group/11`))
-            visaList.push(await webGet(`/visa/group/12`))
-            visaList.push(await webGet(`/visa/group/13`))
-            visaList.push([...(await webGet<Array<{}>>(`/visa/group/14`))!,...(await webGet<Array<{}>>(`/visa/group/16`))!])
-            visaList.push(await webGet(`/visa/group/15`))
+            visaList.push(await webGet<Array<Visa>>(`/visa/group/hot`))
+            visaList.push(await webGet<Array<Visa>>(`/visa/group/11`))
+            visaList.push(await webGet<Array<Visa>>(`/visa/group/12`))
+            visaList.push(await webGet<Array<Visa>>(`/visa/group/13`))
+            visaList.push([...(await webGet<Array<Visa>>(`/visa/group/14`))!, ...(await webGet<Array<Visa>>(`/visa/group/16`))!])
+            visaList.push(await webGet<Array<Visa>>(`/visa/group/15`))
 
             // @ts-ignore
             this.setData({ visaList: visaList })
-            //修复vant-tab渲染延时错误
-            // @ts-ignore
-            setTimeout(() => { this.setData({ show: true }) }, 2500)
         }
     }
 });
