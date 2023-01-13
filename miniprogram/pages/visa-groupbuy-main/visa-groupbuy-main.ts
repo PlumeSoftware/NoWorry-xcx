68a50fcd24f1be31cb063526c4499de256644b2d
Wei-Lei-Qian-Hang <1004259264@qq.com>
2023-01-13 21:21:48 +0800

    "feature:团购页面绘制"

--- miniprogram/pages/visa-groupbuy-main/visa-groupbuy-main.ts
+++ miniprogram/pages/visa-groupbuy-main/visa-groupbuy-main.ts
@@ -9,6 +9,7 @@ import Toast from '@vant/weapp/toast/toast';
 import { Cart } from "../../entity/cart";
 import { culFavFromCarts } from "../../utils/cart";
 
+
 Page({
     data: {
         //page 参数
@@ -18,12 +19,38 @@ Page({
         cityIndex: 0,
 
         //商品详细参数
-        commodity: [] as Cart[],
-        favourable: [] as Array<{}>
+        commodity: [] as Cart,
+        favourable: [] as Array<{}>,
+        orderGroup: [] as any
     },
 
     async onShow() {
         const pages = getCurrentPages()
-        const commodityId = pages[pages.length - 1].options.commodityId
+        // const orderGroupId = pages[pages.length - 1].options.orderGroupId;
+        const orderGroupId = 12111
+        const orderGroup = await webGet<any>(`/order/group/info/${orderGroupId}`);
+        orderGroup.select = true;
+
+        console.log(orderGroup, orderGroup.parters)
+        // commodity.select = true
+        this.setData({ commodity: orderGroup as Cart, orderGroup: orderGroup })
+
+        this.culFav()
+        switch (this.data.commodity.commodityType) {
+            case 11: this.setData({ citiesArray: ['伦敦', '曼彻斯特', "爱丁堡"] }); break;
+            case 13: this.setData({ citiesArray: ['伦敦', '贝尔法斯特'] }); break;
+            case 14: this.setData({ citiesArray: ['伦敦'] }); break;
+            case 15: this.setData({ citiesArray: ['线上办理', '线下办理'] }); break;
+        }
+    },
+
+    async culFav() {
+        const commodity = this.data.commodity;
+        const fav = await culFavFromCarts([commodity]);
+        this.setData({ favourable: fav })
+        if (!fav || fav.length == 0) Toast({ type: 'fail', message: '团购量过少~', duration: 2000 });
+    },
+    genGroupBuy() {
+        wx.showModal({ title: '提示', content: "你的团购码是" })
     }
 });
