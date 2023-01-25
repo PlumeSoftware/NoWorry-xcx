b967cbedc6148b62df84282b33e7fe92994137df
-h <corfer@yeah.net>
2023-01-26 06:51:54 +0800

    fix: 显示错误修复

--- miniprogram/pages/cart-settle/cart-settle.ts
+++ miniprogram/pages/cart-settle/cart-settle.ts
@@ -98,15 +98,6 @@ Page({
         //加入优惠券计算
         favourableTotal += this.data.favdetail.value
 
-        if (total - favourableTotal < 1) {
-            wx.showModal({
-                title: '提示',
-                content: '你真的是在测试吗QAQ',
-                showCancel: false,
-                success: () => wx.navigateBack()
-            })
-        }
-
         total = Number(total.toFixed(2))
         totalCNY = Number((8.35 * (total - favourableTotal)).toFixed(2));
 
