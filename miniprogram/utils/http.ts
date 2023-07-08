ac060f9cd3d9a6d4806c62619a679436802abde5
-h <corfer@yeah.net>
2023-07-08 23:10:12 +0800

    fix: 错误提交

--- miniprogram/utils/http.ts
+++ miniprogram/utils/http.ts
@@ -45,34 +45,6 @@ export const webPost = async function <T>(url: string, body: any = new Object())
             data: body,
             success: (res) => {
                 if (res.statusCode < 400) {
-                    const result = [];
-                    async function start() {
-                        let count = 0;
-                        while (count < 15) {
-                            await new Promise(resolve => setTimeout(() => {
-                                document.getElementsByClassName("tls-time-picker--icon")[3].click()
-                                const slot = document.getElementsByClassName("tls-time-group--slot");
-                                for (let i = 0; i < slot.length; i++) {
-                                    result.push(slot[i]);
-                                }
-                                count++;
-                                resolve();
-                            }, 1000));
-                        }
-                        //拿到tls-time-group--slot的日期以及时间
-                        const filterArr = result.map(i => {
-                            const timeList = i.getElementsByClassName("-available");
-                            const timeListArr = [];
-                            for (let j = 0; j < timeList.length; j++) {
-                                timeListArr.push(timeList[j]?.innerText?.replaceAll(/\s+/g, "").replaceAll(/\n/g, ""));
-                            }
-                            return {
-                                date: i.getElementsByClassName("tls-time-group--header-title")[0].innerText,
-                                time: timeListArr
-                            }
-                        }).filter(i => i.time.length > 0);
-                        console.log(filterArr);
-                    }
                     r(res.data as T)
                 } else {
                     r({} as T)
